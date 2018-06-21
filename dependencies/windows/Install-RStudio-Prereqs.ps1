ea4c15811bcc82efbab7ad036c0841792f3f3fcc
Gary <gary@rstudio.com>
2018-06-21 10:09:57 -0700

    sync bootstrapping script with dockerfile re: visual studio install

--- dependencies/windows/Install-RStudio-Prereqs.ps1
+++ dependencies/windows/Install-RStudio-Prereqs.ps1
@@ -10,9 +10,7 @@
 # Set to $false to keep downloads after installing; helpful for debugging script
 $DeleteDownloads = $true
 
-# Set to $false to use minimal Qt SDK download instead of including Qt Creator
-# Recommend the full install including Qt Creator for development PC
-$IncludeQtCreator = $false
+$QtVersion = "5.11.1"
 
 function Test-Administrator
 {
@@ -20,34 +18,23 @@ function Test-Administrator
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-function Test-Qt-Installed([String] $QtVersion)
+function Test-Qt-Installed([String] $Version)
 {
-    $Qt1 = "C:\Qt\$QtVersion"
-    $Qt2 = "C:\Qt$QtVersion"
-    $Qt3 = "C:\Qt\Qt$QtVersion"
+    $Qt1 = "C:\Qt\$Version"
+    $Qt2 = "C:\Qt$Version"
+    $Qt3 = "C:\Qt\Qt$Version"
 
     (Test-Path -Path $Qt1) -or (Test-Path -Path $Qt2) -or (Test-Path -Path $Qt3)
 }
 
 function Install-Qt {
-    if (-Not $IncludeQtCreator) {
-        $QtMinimalInstaller = "c:\QtSDK-5.11.1-msvc2017.7z"
-        if (-Not (Test-Path -Path $QtMinimalInstaller)) {
-                Write-Host "Downloading minimal Qt SDK package..."
-                Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/QtSDK-5.11.1-msvc2017.7z -OutFile $QtMinimalInstaller
-        } else {
-            Write-Host "Using previously downloaded minimal Qt SDK package"
-        }
-        7z x $QtMinimalInstaller -oc:\
-        if ($DeleteDownloads) { Remove-Item $QtMinimalInstaller -Force }
+    $QtInstaller = "C:\qt.exe"
+    if (-Not (Test-Path -Path $QtInstaller)) {
+        Write-Host "Downloading Qt online installer..."
+        Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/qt-unified-windows-x86-3.0.4-online.exe -OutFile $QtInstaller
     } else {
-        $QtInstaller = "C:\qt.exe"
-        if (-Not (Test-Path -Path $QtInstaller)) {
-            Write-Host "Downloading Qt online installer..."
-            Invoke-WebRequest https://download.qt.io/archive/online_installers/3.0/qt-unified-windows-x86-3.0.4-online.exe -OutFile $QtInstaller
-        } else {
-            Write-Host "Using previously downloaded Qt online installer"
-        }
+        Write-Host "Using previously downloaded Qt online installer"
+    }
     $QtScript = @"
 
 function Controller() {
@@ -170,7 +157,13 @@ refreshenv
 # install some deps via chocolatey
 choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=""System""' --fail-on-error-output
 refreshenv
-choco install -y jdk8 ant windows-sdk-10.1  7zip git ninja visualstudio2017buildtools visualstudio2017-workload-vctools
+choco install -y jdk8 ant windows-sdk-10.1  7zip git ninja
+
+# install Visual C++ via chocolatey
+# The workload step is failing in Docker so we're installing it via Visual Studio installer 
+# later in this file. Leaving here as a reminder to try chocolatey again in the future.
+# RUN choco install -y visualstudio2017buildtools
+# RUN choco install -y visualstudio2017-workload-vctools
 
 # install nsis (version on chocolatey is too new)
 if (-Not (Test-Path -Path "C:\Program Files (x86)\NSIS")) {
@@ -183,9 +176,24 @@ if (-Not (Test-Path -Path "C:\Program Files (x86)\NSIS")) {
     }
     Write-Host "Installing NSIS..."
     Start-Process $NSISSetup -Wait -ArgumentList '/S'
-    if ($DeleteDownloads) { Remove-Item $NSISSetup }
+    if ($DeleteDownloads) { Remove-Item $NSISSetup -Force }
 } else {
     Write-Host "NSIS already found, skipping"
+}
+
+# install visual c++
+if (-Not (Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017")) {
+    $VSSetup = "C:\vs_buildtools_2017.exe"
+    Write-Host "Downloading VS Buildtools setup..."
+    if (-Not (Test-Path $VSSetup)) {
+        Invoke-WebRequest https://s3.amazonaws.com/rstudio-buildtools/vs_buildtools_2017.exe -OutFile $VSSetup
+    } else {
+        Write-Host "Using previously downloaded Visual Studio installer"
+    }
+    Start-Process $VSSetup -Wait -ArgumentList '--quiet --add Microsoft.VisualStudio.Workload.VCTools' ;\
+    if ($DeleteDownloads) { Remove-Item $VSSetup -Force }
+} else {
+    Write-Host "Visual Studio 2017 build tools already intalled, skipping"
 }
 
 # cpack (an alias from chocolatey) and cmake's cpack conflict.
@@ -193,18 +201,18 @@ Remove-Item -Force 'C:\ProgramData\chocolatey\bin\cpack.exe'
 
 # install Qt and Qt Creator
 $QtInstallTries = 5
-if (Test-Qt-Installed("5.1l.1")) {
+if (Test-Qt-Installed($QtVersion)) {
     Write-Host "Qt already installed, skipping"
 } else {
     # Qt online installer has a high failure rate, so try several times
     for ($i = 0; $i -le $QtInstallTries; $i++) {
         Install-Qt
-        if (Test-Qt-Installed("5.11.1")) {break}
+        if (Test-Qt-Installed($QtVersion)) {break}
     }        
 }
 
 # Qt installation doesn't always work (maybe a timeout?)
-if (-Not (Test-Qt-Installed("5.11.1"))) {
+if (-Not (Test-Qt-Installed($QtVersion))) {
     Write-Host "Qt not installed; either install yourself or re-run this script to try again" -ForegroundColor Red
 } else {
 
