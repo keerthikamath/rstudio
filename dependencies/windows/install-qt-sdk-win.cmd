e42749914caee53b0d5c5a02ac7694b75017c9f0
Gary <gary@rstudio.com>
2018-06-22 12:00:50 -0700

    fixes to windows qt installer script

--- dependencies/windows/install-qt-sdk-win.cmd
+++ dependencies/windows/install-qt-sdk-win.cmd
@@ -6,7 +6,7 @@ set QT_VERSION=5.11.1
 set QT_FILE=qt-unified-windows-x86-3.0.4-online.exe
 
 call :DetectQt foundQt
-if "!foundQt! == "false" (
+if "!foundQt!" == "false" (
   wget %WGET_ARGS% %BASEURL%%QT_FILE%
   echo "Installing Qt, this will take a while."
   echo "Ignore warnings about QtAccount credentials."
@@ -15,12 +15,15 @@ if "!foundQt! == "false" (
   del %QT_FILE%
 )
 call :DetectQt foundQt
-if "!foundQt! == "false" (
+if "!foundQt!" == "false" (
   echo Qt installation failed, please re-run this script to try again.
   echo Or you can manually install with the Qt online installer and select
   echo the 64-bit Visual Studio 2017 and QtWebEngine components of
   echo %QT_VERSION%.
   exit /b 1
+)
+else (
+  echo Qt %QT_VERSION% installed.
 )
 exit /b 0
 
@@ -32,7 +35,7 @@ set QT_SDK_DIR3=C:\Qt\%QT_VERSION%
 if not exist %QT_SDK_DIR% (
   if not exist %QT_SDK_DIR2% (
     if not exist %QT_SDK_DIR3% (
-      set "~1=false"
+      set "%~1=false"
     )
   )
 )
